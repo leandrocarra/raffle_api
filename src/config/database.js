4123c79ab1b4d84eaa2bf24e41447b8557d5bb48
Alf <leandro.carra@luizalabs.com>
2018-10-10 11:04:41 -0300

    add amigos

--- src/config/database.js
+++ src/config/database.js
@@ -2,4 +2,5 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://carra:q1w2e3r4t5@ds263832.mlab.com:63832/todolist')
+module.exports = mongoose.connect('mongodb://carra:q1w2e3r4t5@ds163402.mlab.com:63402/raffle')
+// Familia :  module.exports = mongoose.connect('mongodb://carra:q1w2e3r4t5@ds263832.mlab.com:63832/todolist')
